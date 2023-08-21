3c4e1cdd0e4d1635617ca564f4aaefcd52bfd608
Anna <73854503+Blossom0204@users.noreply.github.com>
2023-08-21 20:21:29 +0300

    feat:code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+  const reverseString = n.toString().split('').reverse().join('');
+  return parseInt(reverseString);
+};
